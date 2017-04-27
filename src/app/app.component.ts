535ea911999277adc57c442c6fe9fb88cb3dda54
Syed Ali Abbas Shah (01322) <ali.abbas@nxb.com.pk>
2017-04-27 17:56:10 +0500

    css faults covered in previous branch and adding image added article class and view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,10 @@ export class AppComponent {
   constructor(){
     this.articles = [
       //defining instances in array objects
-      new Article('Erlang development','Erlang is fastest in network communication') ,
-      new Article('Elixir development','Elixer is written as wrapper over erlang') ,
-      new Article('Phoenix development','Phoenix is framework of angular') ,
-      new Article('Angular JS development','Dynamic web development') ,
+      new Article('Erlang development','Erlang is fastest in network communication',"http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png") ,
+      new Article('Elixir development','Elixer is written as wrapper over erlang',"http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png") ,
+      new Article('Phoenix development','Phoenix is framework of angular',"http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png") ,
+      new Article('Angular JS development','Dynamic web development',"http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png") ,
    ]
   }
 }
